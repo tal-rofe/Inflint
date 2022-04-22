cf0fd9ee5771561fa4787437cf63876d4a7ae47b
Tal Rofe <tal@vinyldepository.com>
2022-04-22 22:43:07 +0300

    refactor: 💡 handle fatal errors in bin
    handle fatal errors in bin

--- bin/inflint.js
+++ bin/inflint.js
@@ -3,4 +3,19 @@
 // to use V8's code cache to speed up instantiation time
 require('v8-compile-cache');
 
+function onFatalError(error) {
+	process.exitCode = 1;
+
+	const { version } = require('../package.json');
+	const message = error.message || error;
+
+	console.error(`
+Something went wrong! :(
+Inflint: ${version}
+${message}`);
+}
+
+process.on('uncaughtException', onFatalError);
+process.on('unhandledRejection', onFatalError);
+
 require('../dist/index');
