ccfa7b92b863b87c096f05d1da8e201d1afa77c0
Tal Rofe <tal@vinyldepository.com>
2022-04-25 20:02:59 +0300

    refactor: 💡 use czvinyl and inflint typescript configuration files
    use czvinyl and inflint typescript configuration files

--- inflint.config.ts
+++ inflint.config.ts
@@ -1,6 +1,10 @@
-module.exports = {
+import { Config } from 'inflint';
+
+const inflintConfig: Config = {
 	rules: {
 		'src/**/*': [2, 'kebab-case.point'],
 		'{assets,scripts,bin}/**/*': [2, 'kebab-case'],
 	},
 };
+
+export default inflintConfig;
