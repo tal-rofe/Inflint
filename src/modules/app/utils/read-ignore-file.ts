cb36a7a91c8b5080315bf9d9ffcc28757a13e486
Tal Rofe <tal@exlint.io>
2022-07-08 19:43:49 +0300

    fix: 🐞 using relative and absolute paths in read ignore file
    using relative and absolute paths in read ignore file

--- src/modules/app/utils/read-ignore-file.ts
+++ src/modules/app/utils/read-ignore-file.ts
@@ -6,10 +6,16 @@ import CLILoggerModule from '@/shared/modules/cli-logger';
 import { DEFAULT_PATH } from '../models/ignore-file';
 
 export const readIgnoreFile = async (filePath?: string) => {
-	const ignoreFilePath = path.join(process.cwd(), filePath ?? DEFAULT_PATH);
+	let absoluteIgnorePath: string;
+
+	if (!filePath) {
+		absoluteIgnorePath = DEFAULT_PATH;
+	} else {
+		absoluteIgnorePath = path.isAbsolute(filePath) ? filePath : path.join(process.cwd(), filePath);
+	}
 
 	try {
-		const fileContent = await fs.readFile(ignoreFilePath, 'utf8');
+		const fileContent = await fs.readFile(absoluteIgnorePath, 'utf8');
 
 		return fileContent.split(/\r?\n/);
 	} catch {
