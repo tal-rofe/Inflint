0e54a44f383a0be0b92cb9f491f2ae6ac349fe4f
tal-rofe <tal@exlint.io>
2023-04-14 13:51:54 +0300

    breaking: 💥 set release
    set release

--- release.config.cjs
+++ release.config.cjs
@@ -2,7 +2,18 @@ const config = {
 	branches: ['main'],
 	repositoryUrl: 'git+https://github.com/Exlint/Inflint',
 	plugins: [
-		'@semantic-release/commit-analyzer',
+		[
+			'@semantic-release/commit-analyzer',
+			{
+				preset: 'angular',
+				releaseRules: [
+					{ type: 'feat', release: 'minor' },
+					{ type: 'fix', release: 'patch' },
+					{ type: 'perf', release: 'patch' },
+					{ type: 'breaking', release: 'major' },
+				],
+			},
+		],
 		'@semantic-release/release-notes-generator',
 		'@semantic-release/changelog',
 		'@semantic-release/npm',
